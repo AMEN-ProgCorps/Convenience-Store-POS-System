31926d90a7e6c991082761b4c8adc6084f13a24d
MacCloud <kuzabala@my.cspc.edu.ph>
2025-05-24 20:57:11 +0800

    1

---
+++ php/employee/Estaf.php
@@ -0,0 +1,70 @@
+<?php
+include '../import/database.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ToMart</title>
+        <link rel="icon" href="../../style/ToMart_Logo.png" type="image/x-icon">
+        <link rel="stylesheet" href="../../style/index.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <script src="../../script/lan.js" defer></script>
+    </head>
+    <body>
+        <div class="body_container">
+            <div class="side-bar"><!--Side bar for tabs-->
+                <div class="logo-container">
+                    <div class="logo-icon"></div>
+                    <div class="logo-label">ToMart</div> 
+                </div>
+                <div class="username-container">
+                        Hello! <?php echo isset($_SESSION['user_name']) ? htmlspecialchars($_SESSION['user_name']) : 'Customer'; ?>
+                </div>
+                <div class="tab-container">
+                    <div class="items-tab tab" onclick="toggleMenu('Inventory')">
+                        <div class="tab-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M160 448a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zM160 896a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32z"/></svg>
+                        </div>
+                        <div class="tab-label">Inventory</div>
+                    </div>
+                    <div class="items-tab tab" onclick="toggleMenu('Search')">
+                        <div class="tab-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M160 448a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zM160 896a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32z"/></svg>
+                        </div>
+                        <div class="tab-label">Search Items</div>
+                    </div>
+                    <div class="order-tab tab" onclick="toggleMenu('Cashier')">
+                        <div class="tab-icon"><i class="fas fa-shopping-cart"></i></div>
+                        <div class="tab-label">Cashier</div>
+                    </div>
+                    <div class="items-tab tab" onclick="toggleMenu('Accounts')">
+                        <div class="tab-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M160 448a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zM160 896a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32z"/></svg>
+                        </div>
+                        <div class="tab-label">Accounts</div>
+                    </div>
+                    <div class="items-tab tab active">
+                        <div class="tab-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M160 448a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V160.064a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32V416a32 32 0 0 1-32 32zM160 896a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32zm448 0a32 32 0 0 1-32-32V608a32 32 0 0 1 32-32h255.936a32 32 0 0 1 32 32v256a32 32 0 0 1-32 32z"/></svg>
+                        </div>
+                        <div class="tab-label">Staff</div>
+                    </div>
+                    <div class="logout-tab tab" onclick="window.location.href='?logout=1';">
+                        <div class="tab-icon"><i class="fa-solid fa-circle-user"></i></div>
+                        <div class="tab-label">Logout</div>
+                    </div>
+                </div>
+            </div>
+            <div class="center-bar"><!--Main bar for for stuffs-->
+                
+            </div>
+            <div class="cart-bar active"><!--Side bar for Cart-->
+           
+            </div> 
+        </div>
+    </body>
+</html>
